32f649f6bb504f1546474c66b8736241e9961b2e
ahari@tavisca.in <ahari@tavisca.in>
2019-08-14 18:23:31 +0530

    new change

--- NewWebApi/Program.cs
+++ NewWebApi/Program.cs
@@ -19,6 +19,7 @@ namespace NewWebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls(urls:"http://*:8021");
     }
 }
