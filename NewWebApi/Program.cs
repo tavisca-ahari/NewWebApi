267342900863f8313b0a61bc577b06344c220e06
ahari@tavisca.in <ahari@tavisca.in>
2019-08-16 10:38:20 +0530

    added dockerfile

--- NewWebApi/Program.cs
+++ NewWebApi/Program.cs
@@ -19,7 +19,7 @@ namespace NewWebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls(urls:"http://*:8021");
+                .UseStartup<Startup>();
+              
     }
 }
